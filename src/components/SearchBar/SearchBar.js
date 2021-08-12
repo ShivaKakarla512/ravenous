3d185a8f9a5bd5e5d915086847227a832db30199
Shiva Kakarla <shiva.kakarla512@gmail.com>
2021-08-12 12:18:59 +0530

    First commit

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,82 @@
+import React from 'react';
+import './SearchBar.css';
+
+class SearchBar extends React.Component {
+    constructor (props) {
+        super(props);
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match',
+        };
+        this.sortByOptions = {
+            'Best Match' : 'best_match',
+            'Highest Rated' : 'rating',
+            'Most Reviewed' : 'review_count',
+        };
+    }
+
+    getSortByClass (sortByOption) {
+        if (sortByOption === this.state.sortBy) {
+            return 'active';
+        } else {
+            return '';
+        }
+    }
+
+    handleSortByChange (sortByOption) {
+        this.setState({sortBy : sortByOption});
+    }
+
+    handleTermChange (event) {
+        this.setState({term : event.target.value})
+    }
+
+    handleLocationChange (event) {
+        this.setState({location : event.target.value})
+    }
+
+    handleSearch (event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy);
+        event.preventDefault();
+    }
+
+    renderSortByOptions () {
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return (
+                <li 
+                    onClick={this.handleSortByChange.bind(this, sortByOptionValue)} 
+                    className={this.getSortByClass(sortByOptionValue)} 
+                    key={sortByOptionValue}
+                >
+                    {sortByOption}
+                </li>
+            )
+        });
+    }
+
+    render () {
+        return (
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                        {this.renderSortByOptions()}
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+                    <input onChange={this.handleLocationChange} placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a onClick={this.handleSearch}>Let's Go</a>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
---
+++ src/util/Yelp.js
@@ -0,0 +1,32 @@
+const apiKey = 'Tv-Qghle6Y58QT-j0LuUWXGmfn2WxXiFO3bNkOTFzBCWD1cJDqO3dr8v5vcc1cqTHf2pk7CI0FqNI7vncH15edoXnOhZhh4wRbFQfM7qphUvQzyftzv3lvxEunDtXXYx';
+
+let Yelp = {
+    search (term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
+            headers: {
+                Authorization : `Bearer ${apiKey}`,
+            }
+        }).then(response => {
+            return response.json();
+        }).then(jsonResponse => {
+            if (jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    return {
+                        id : business.id,
+                        imageSrc : business.image_url,
+                        name: business.name,
+                        address: business.location.address1,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories[0].title,
+                        rating: business.rating,
+                        reviewCount: business.review_count,
+                    }
+                });
+            }
+        })
+    }
+};
+
+export default Yelp;
